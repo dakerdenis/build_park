aca6bb2eb7b4e3bc03a2765140b04a5d27a16c6e
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-10-25 15:26:52 +0400

    фввув

--- myproject/resources/js/app.js
+++ myproject/resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header__wrapper');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {  // You can adjust this value to change when the effect starts
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+});
